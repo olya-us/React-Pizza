1d84c6ac7181d69829d8415c16ea4a67d35dc628
Olya Us <olyaus0809@gmail.com>
2022-10-31 13:59:00 +0300

    mockapi.io

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { useState, useEffect } from 'react';
 import {Header, Categories, SortPopup, PizzaBlock} from './components/index';
 
-import pizzas from './assets/pizzas.json';
-
 const App = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch('https://635f9b813e8f65f283b700ad.mockapi.io/items')
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header/>
@@ -15,7 +24,7 @@ const App = () => {
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
             {
-              pizzas.map((obj, i) => (
+              items.map((obj) => (
                 <PizzaBlock {...obj} key={obj.id}/>
               ))
             }  
