3ec5b58a2e8819b13836c28436fe1e191fe333f9
Olya Us <olyaus0809@gmail.com>
2022-11-01 11:23:06 +0300

    Page Cart

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,10 +12,11 @@ export const Home = () => {
           setItems(arr);
           setIsLoading(false);
         });
+        window.scrollTo(0, 0);
     }, []);
 
     return (
-        <>
+        <div className="container">
             <div className="content__top">
                 <Categories/>
                 <SortPopup/>
@@ -27,6 +28,6 @@ export const Home = () => {
                 : items.map((obj) => (<PizzaBlock {...obj} key={obj.id}/>))
                 }  
             </div>
-        </>
+        </div>
     );
 };
